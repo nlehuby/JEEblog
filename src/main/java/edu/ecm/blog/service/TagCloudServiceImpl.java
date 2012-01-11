b9dd1e734951cedfb54ad4df5e639ac126a8babe
Noémie <noemie.lehuby@centrale-marseille.fr>
2012-01-11 18:27:22 +0100

    implémentation de TagCloudService

--- src/main/java/edu/ecm/blog/service/TagCloudServiceImpl.java
+++ src/main/java/edu/ecm/blog/service/TagCloudServiceImpl.java
@@ -1,13 +1,39 @@
 package edu.ecm.blog.service;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.commons.lang.StringUtils;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.ecm.blog.domain.Post;
 import edu.ecm.blog.util.TagCloud;
 
+@Service
 public class TagCloudServiceImpl implements TagCloudService {
+	@Inject
+	private SessionFactory sessionFactory;
 
 	@Override
+	@Transactional
 	public TagCloud buildTagCloud() {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = sessionFactory.getCurrentSession();
+
+		TagCloud nuage = new TagCloud();
+
+		Criteria criteria = session.createCriteria(Post.class);
+		List<Post> liste_post = criteria.list();
+		
+		for (Post post : liste_post) {
+			nuage.add(StringUtils.split(post.getTags(), ","));
+		}
+
+		return nuage;
 	}
 
 }
